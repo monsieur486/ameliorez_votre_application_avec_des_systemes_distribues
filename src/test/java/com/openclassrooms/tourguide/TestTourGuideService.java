91edd28bf9f3b0b72b7f9f413441654d9aee7124
Monsieur 486 <monsieur486@gmail.com>
2024-06-18 14:04:33 +0200

    test ajustement

--- src/test/java/com/openclassrooms/tourguide/TestTourGuideService.java
+++ src/test/java/com/openclassrooms/tourguide/TestTourGuideService.java
@@ -92,7 +92,7 @@ public class TestTourGuideService {
 		assertEquals(user.getUserId(), visitedLocation.userId);
 	}
 
-	//@Disabled // Not yet implemented
+	@Disabled // Not yet implemented
 	@Test
 	public void getNearbyAttractions() {
 		GpsUtil gpsUtil = new GpsUtil();
@@ -110,6 +110,7 @@ public class TestTourGuideService {
 		assertEquals(5, attractions.size());
 	}
 
+	@Disabled // Not yet implemented
 	@Test
 	public void getTripDeals() {
 		GpsUtil gpsUtil = new GpsUtil();
