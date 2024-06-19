6eaa1ac22f30632d32f28666cc3277a69381ddcb
Monsieur 486 <monsieur486@gmail.com>
2024-06-19 14:42:32 +0200

    add deployment files

--- DeploymentFiles/Release/Linux/update.sh
+++ DeploymentFiles/Release/Linux/update.sh
@@ -1,2 +1,6 @@
-rm latest/tourguide-stable-release.jar
+FILE="$PWD"/tourguide-stable-release.jar
+if [ -f "$FILE" ]; then
+    rm $FILE
+fi
+
 wget https://github.com/monsieur486/ameliorez_votre_application_avec_des_systemes_distribues/releases/download/latest/tourguide-stable-release.jar
