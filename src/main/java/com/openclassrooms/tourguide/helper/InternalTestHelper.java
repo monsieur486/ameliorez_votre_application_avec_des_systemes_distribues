af7d677c3b02f9f7b70cca1119f1c0d41ce06bea
monsieur486 <monsieur486@gmail.com>
2026-06-22 10:05:58 +0200

    reformat code

--- src/main/java/com/openclassrooms/tourguide/helper/InternalTestHelper.java
+++ src/main/java/com/openclassrooms/tourguide/helper/InternalTestHelper.java
@@ -4,14 +4,14 @@ import com.openclassrooms.tourguide.TourGuideConfiguration;
 
 public class InternalTestHelper {
 
-	// Set this default up to 100,000 for testing
-	private static int internalUserNumber = TourGuideConfiguration.NUMBER_OF_USERS;
-	
-	public static void setInternalUserNumber(int internalUserNumber) {
-		InternalTestHelper.internalUserNumber = internalUserNumber;
-	}
-	
-	public static int getInternalUserNumber() {
-		return internalUserNumber;
-	}
+  // Set this default up to 100,000 for testing
+  private static int internalUserNumber = TourGuideConfiguration.NUMBER_OF_USERS;
+
+  public static int getInternalUserNumber() {
+    return internalUserNumber;
+  }
+
+  public static void setInternalUserNumber(int internalUserNumber) {
+    InternalTestHelper.internalUserNumber = internalUserNumber;
+  }
 }
